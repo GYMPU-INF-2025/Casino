ccbafe9ae35185e63752bb83a922164e743f629b
Rayakame <github.android206@passmail.net>
2025-04-30 10:42:42 +0000

    refactor(ci): fix ruff errors (feature/ruff)

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,22 @@
-import sanic
+"""Entrypoint for our application."""
 
-from backend.serialization import serialize, deserialize
-from backend.models.responses import Success, Test
+from __future__ import annotations
+
+import sanic
+from sanic.log import logger
+
+from backend.models.responses import Success
+from backend.models.responses import Test
+from backend.serialization import deserialize
+from backend.serialization import serialize
 
 app = sanic.Sanic("Casino")
+
 
 @app.post("/")
 @serialize()
 @deserialize()
 async def hello_world(_: sanic.Request, body: Test) -> Success:
-    print(body.test)
+    """Hello World endpoint to test if the server is running."""
+    logger.info(body.test)
     return Success()
-
