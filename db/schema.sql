c0ad75ba6f8b123e9c0a1833e93de0cdbe4b63ef
Christopher <github.android206@passmail.net>
2025-06-09 22:10:18 +0200

    reformat schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,23 +1,7 @@
 CREATE TABLE IF NOT EXISTS users
 (
-    id
-    bigserial
-    PRIMARY
-    KEY
-    NOT
-    NULL,
-    username
-    text
-    NOT
-    NULL,
-    password
-    text
-    NOT
-    NULL,
-    money
-    integer
-    DEFAULT
-    0
-    NOT
-    NULL
+    id       BIGSERIAL PRIMARY KEY NOT NULL,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    money    INTEGER DEFAULT 0 NOT NULL
 )
