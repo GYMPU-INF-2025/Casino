696dda81ce13016cf11d4aa0c78ce5e343369cd5
Elijah <iewhna@gmail.com>
2025-07-12 01:25:50 +0200

    chickengame

--- backend/internal/ws/websocket_endpoints.py
+++ backend/internal/ws/websocket_endpoints.py
@@ -20,14 +20,17 @@ class WebsocketEndpointsManager:
 
     def add_lobby(self, game_lobby_type: type[GameLobbyBase]) -> None:
         endpoint = WebsocketManager[game_lobby_type](game_lobby_type)
-        self._app.add_websocket_route(endpoint.handle_websocket, f"/{game_lobby_type.endpoint()}/<lobby_id:str>")
+        self._app.add_websocket_route(endpoint.handle_websocket, f"/{game_lobby_type.endpoint()}/<lobby_id:str>",
+            name=game_lobby_type.endpoint() + 'websocket',)
         self._app.add_route(
             handler=typing.cast("RouteHandler", endpoint.list_lobbys),
             uri=f"/{game_lobby_type.endpoint()}/",
             methods=frozenset({"GET"}),
+            name=game_lobby_type.endpoint() + 'list_lobby',
         )
         self._app.add_route(
             handler=typing.cast("RouteHandler", endpoint.create_lobby),
             uri=f"/{game_lobby_type.endpoint()}/",
             methods=frozenset({"POST"}),
+            name=game_lobby_type.endpoint() + 'create_lobby',
         )
