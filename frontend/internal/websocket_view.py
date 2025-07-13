85d598faf31688ad980ea0e273d8f39e99eda060
rayakame <github.android206@passmail.net>
2025-07-14 00:04:13 +0200

    docs

--- frontend/internal/websocket_view.py
+++ frontend/internal/websocket_view.py
@@ -44,6 +44,17 @@ class _WebsocketViewMeta(abc.ABCMeta):
 
 
 class WebsocketView(BaseGameView, metaclass=_WebsocketViewMeta):
+    """Base class for game views that require a websocket connection to the server. The ws connection to the server
+    is established after an instance of the class got created by using the provided game_mode and lobby id.
+
+    It starts an WebsocketThread which listens for new websocket messages and pulls them out of the shared queue.
+    This class also has a mapping for storing "event handlers". This mapping is populated after an instance of this
+    class got created by looking for functions with an __event_type__ attribute. This is added when using the
+    `add_event_listener` decorator. You can also use the `add_event_callback` function of this class to add new
+    event handlers.
+
+    Authors: Christopher
+    """
     def __init__(self, window: MainWindow, game_mode: c.GameModes, lobby_id: str) -> None:
         super().__init__(window=window)
         self._game_mode = game_mode
@@ -62,6 +73,10 @@ class WebsocketView(BaseGameView, metaclass=_WebsocketViewMeta):
         )
 
     def __post__init__(self) -> None:
+        """Function called after an instance of this class got created.
+
+        This starts the websocket thread and looks for event listeners.
+        """
         for attr_name in dir(self):
             maybe_listener = getattr(self, attr_name)
 
@@ -70,11 +85,13 @@ class WebsocketView(BaseGameView, metaclass=_WebsocketViewMeta):
         self.start()
 
     def __on_ws_disconnect(self) -> None:
+        """Called when the websocket disconnects."""
         self._show_main_menu.set()
 
     def add_event_callback(
         self, event_type: type[events.BaseEvent], callback: collections.abc.Callable[[events.BaseEvent], None]
     ) -> None:
+        """Function that adds an event handler/callback."""
         logger.debug(
             "subscribing callback 'async def %s%s' to event-type %s.%s",
             getattr(callback, "__name__", "<anon>"),
@@ -90,15 +107,20 @@ class WebsocketView(BaseGameView, metaclass=_WebsocketViewMeta):
         self._ws_thread.register_event(event_type)
 
     def start(self) -> None:
+        """Function that starts the websocket thread."""
         self._ws_thread.start()
 
     def send_event(self, event: events.BaseEvent) -> None:
+        """Helper function that sends an event to the websocket thread / server."""
         event_name = event.event_name()
         data = msgspec.to_builtins(event, enc_hook=encode_hook)
         self._ws_thread.dispatch_event(data, event_name)
 
     @typing.override
     def on_update(self, delta_time: float) -> bool | None:
+        """This overrides the arcade `on_update` function. Every time this function gets called it looks for new
+        events in the queue and pulls them out of the queue. It then dispatches them.
+        """
         if self._show_main_menu.is_set():
             self.window.show_main_menu()
         else:
